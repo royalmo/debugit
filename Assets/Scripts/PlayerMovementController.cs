b7bbc6f235bc7de52fc925be057f237bc92c131e
Eric Roy <royalmo6@gmail.com>
2021-01-24 02:19:59 +0100

    I don't know what is it but it doesn't work

---
+++ Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour {
+
+    [SerializeField]
+    KeyCode ForewardKey;
+
+    [SerializeField]
+    KeyCode BackwardKey;
+
+    [SerializeField]
+    KeyCode RightKey;
+
+    [SerializeField]
+    KeyCode LeftKey;
+
+    [SerializeField]
+    KeyCode JumpKey;
+
+    // Start is called before the first frame update
+    private void Start() {
+        Vector3 Forces = GetComponent<Rigidbody>.forces;
+    }
+
+    // Update is called once per frame
+    private void Update() {
+        
+    }
+}
